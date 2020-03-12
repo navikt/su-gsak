947482425ac8ba36c65afddc25aecb4f71f2edf1
Håkon <hakon.arneng.holmstedt@nav.no>
2020-03-12 10:00:53 +0100

    flytter en if bort fra kritisk path og putter den i oppsettet i stedet

--- src/main/kotlin/no/nav/su/gsak/Application.kt
+++ src/main/kotlin/no/nav/su/gsak/Application.kt
@@ -8,6 +8,8 @@ import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import no.nav.su.meldinger.kafka.Topics.SØKNAD_TOPIC
 import no.nav.su.meldinger.kafka.soknad.NySøknad
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import kotlin.system.exitProcess
 
@@ -38,22 +40,31 @@ internal fun Application.suGsak(
 
     val useGSak = environment.config.getProperty("gsak.enabled").toBoolean()
     val meldingsleser = environment.config.kafkaMiljø().meldingsleser(Metrics)
+    val mottak = if (useGSak) AktivtMottak(gsakConsumer, kafkaProducer) else PassivtMottak(LOG)
     launch {
         try {
             while (this.isActive) {
-                meldingsleser.lesMelding<NySøknad> { message ->
-                    if (useGSak) {
-                        val gsakId = gsakConsumer.hentGsak(message)
-                        kafkaProducer.send(message.medSkyggesak(gsakId).toProducerRecord(SØKNAD_TOPIC))
-                    } else {
-                        LOG.info(message.toString())
-                    }
-                }
+                meldingsleser.lesMelding<NySøknad> { nySøknad -> mottak.motta(nySøknad) }
             }
         } catch (e: Exception) {
             LOG.error("Exception caught while processing kafka message: ", e)
             exitProcess(1)
         }
+    }
+}
+
+private sealed class Søknadsmottak() {
+    abstract fun motta(melding: NySøknad)
+}
+private class AktivtMottak(private val gsakConsumer: GsakConsumer, private val kafkaProducer: KafkaProducer<String, String>): Søknadsmottak() {
+    override fun motta(melding: NySøknad) {
+        val gsakId = gsakConsumer.hentGsak(melding)
+        kafkaProducer.send(melding.medSkyggesak(gsakId).toProducerRecord(SØKNAD_TOPIC))
+    }
+}
+private class PassivtMottak(private val log: Logger): Søknadsmottak() {
+    override fun motta(melding: NySøknad) {
+        log.info(melding.toString())
     }
 }
 
